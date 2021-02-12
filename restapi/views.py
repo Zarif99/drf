0b4e46feb52670255eadf4173cbf17aa3e04a555
Zarif <9685440@gmail.com>
2021-02-12 22:11:07 +0500

    APIView

--- restapi/views.py
+++ restapi/views.py
@@ -1,40 +1,44 @@
-# django
-from django.shortcuts import get_object_or_404
-# rest_framework
 from rest_framework import status
-from rest_framework import viewsets
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# app
 from restapi.models import Article
 from restapi.serializers import ArticleSerializer
 
 
-class ArticleList(viewsets.ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+class ArticleList(APIView):
+    def get(self, request):
+        articles = Article.objects.filter(draft=False)
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
 
-    queryset = Article.objects.filter(draft=False)
-    serializer_class = ArticleSerializer
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ArticleDetail(APIView):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
     def get(self, request, pk):
-        article = get_object_or_404(Article, pk=pk, draft=False)
+        article = get_object_or_404(Article, draft=False, pk=pk)
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        article = get_object_or_404(Article, pk=pk, draft=False)
-        serializer = ArticleSerializer(article)
+        article = get_object_or_404(Article, draft=False, pk=pk)
+        serializer = ArticleSerializer(instance=article, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = get_object_or_404(Article, pk=pk)
+        article.draft = True
+        article.save()
+        return Response('Article is deleted')
