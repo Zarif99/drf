335f13a01116831cb265890ee2865d7c49501397
Zarif <9685440@gmail.com>
2021-02-15 12:04:58 +0500

    add

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from restapi.views import ArticleList, ArticleDetail
+from restapi.views import ArticleList, ArticleDetail, ArticleCreate
 
 
 urlpatterns = [
     path('article/', ArticleList.as_view(), name='article_list_url'),
+    path('article/create/', ArticleCreate.as_view(), name='article_list_url'),
     path('article/<int:pk>/', ArticleDetail.as_view(), name='article_detail_url'),
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -1,17 +1,27 @@
 from rest_framework import status
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from restapi.models import Article
 from restapi.serializers import ArticleSerializer
 
+authentication_classes = [SessionAuthentication, BasicAuthentication]
+
 
 class ArticleList(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         articles = Article.objects.filter(draft=False)
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
+
+
+class ArticleCreate(APIView):
+    permission_classes = [IsAdminUser]
 
     def post(self, request):
         serializer = ArticleSerializer(data=request.data)
@@ -23,6 +33,9 @@ class ArticleList(APIView):
 
 
 class ArticleDetail(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAdminUser]
+
     def get(self, request, pk):
         article = get_object_or_404(Article, draft=False, pk=pk)
         serializer = ArticleSerializer(article)
